65f1174c29ce64e59c5e58e5eed04b2c264a1231
9seebird <bonse88@hs.ac.kr>
2022-01-17 10:30:07 +0900

    서버 연결

---
+++ Socket/src/network/Cilent.java
@@ -0,0 +1,24 @@
+package network;
+
+import javax.print.attribute.standard.Severity;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Cilent {
+    //클라이언트
+    public static void main(String[] args) {
+
+       //소켓 선언
+       Socket socket = null;
+
+       // 소켓 생성
+        try {
+            socket = new Socket("localhost",7777) //127.0.0.1 루프백
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ Socket/src/network/Server.java
@@ -0,0 +1,29 @@
+package network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    //서버
+    public static void main(String[] args) {
+
+        //서버소켓 선언
+        ServerSocket serverSocket = null;
+
+
+        try {
+            //서버소켓 생성
+            serverSocket = new ServerSocket(7777); //포트번호만 적어서 생성성
+
+            System.out.println("서버 대기 중...");
+
+            //서버소켓 accept (대기상태)
+            Socket socket = serverSocket.accept();
+
+            System.out.println("클라이언트 연결 완료");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
